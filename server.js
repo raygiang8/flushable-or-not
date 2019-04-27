6fcea98f677b72553edf36910ed75670ce4fbae4
Ray <raymondgiang@gmail.com>
2019-04-27 19:34:14 -0400

    testing changes to server

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'build', '/'));
     });
 }
