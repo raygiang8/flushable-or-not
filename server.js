b17e1865543baa01e687d682b5035e9c85459aa7
Ray <raymondgiang@gmail.com>
2019-04-27 19:17:25 -0400

    add to server.js

--- server.js
+++ server.js
@@ -72,3 +72,11 @@ io.on("connection", function (socket) {
         io.sockets.emit("gameOver");
     });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
