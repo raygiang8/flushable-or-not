455d37523187946e7dea56bf99b9b5d3aa04c82e
Ray <raymondgiang@gmail.com>
2019-04-27 11:57:32 -0400

    random flushables logic

--- src/App.js
+++ src/App.js
@@ -6,15 +6,38 @@ import ImageArea from './ImageArea';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPlaying: null,
+      currentChallenge: null,
+    };
 
+    // Connect socket
     const socket  = io.connect();
+
+    /* Challenges:
+        0: Find the flushable
+        1: Find the unflushable
+    */
+    this.challenges = [0, 1];
+
+    // 0 = empty, 10 = full
+    this.toiletOverflow = 0;
+  }
+
+  newGame = () => {
+    let challenge = Math.floor(Math.random() * this.challenges.length);
+    this.setState({
+      isPlaying: true,
+      currentChallenge: challenge,
+    });
   }
 
   render() {
     return (
       <div>
-        Hello
-        <ImageArea />
+        <h1>Flushable or Not</h1>
+        <button id="start-game" onClick={this.newGame}>Start Game</button>
+        <ImageArea isPlaying={this.state.isPlaying} currChallenge={this.state.currentChallenge} />
       </div>
     );
   }
