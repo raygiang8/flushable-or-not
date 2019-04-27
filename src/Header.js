b75ce227b3b1f4a3793200fcdd6a5f73df5c948c
Ray <raymondgiang@gmail.com>
2019-04-27 18:47:31 -0400

    fixed header

--- src/Header.js
+++ src/Header.js
@@ -4,30 +4,9 @@ import TimerBar from './TimerBar';
 class Header extends Component {
   constructor(props) {
     super(props);
-
-    this.challenge = null;
   }
 
   render() {
-    if(this.props.currChallenge === 0) {
-      this.challenge = "Find the Flushable";
-    }
-    else if(this.props.currChallenge === 1) {
-      this.challenge = "Find the Unflushable";
-    }
-    else if(this.props.currChallenge === 2) {
-      this.challenge = "Find the Not Flushable";
-    }
-    else if(this.props.currChallenge === 3) {
-      this.challenge = "Find the Not Unflushable";
-    }
-    else if(this.props.currChallenge === 4) {
-      this.challenge = "Find the Flushable";
-    }
-    else if(this.props.currChallenge === 5) {
-      this.challenge = "Find the Unflushable";
-    }
-
     return (
       <div className="header">
         <div className="header__timer">
@@ -42,7 +21,7 @@ class Header extends Component {
             <h3 className="header__heading">Time to Choose: {this.props.timer} seconds</h3>
           </div>
           <div className="">
-            <h3 className="header__heading">Toilet Clog Level: {props.toiletLevel}</h3>
+            <h3 className="header__heading">Toilet Clog Level: {this.props.toiletLevel}</h3>
           </div>
         </div>
         {/*<TimerBar time={this.props.timer} />*/}
